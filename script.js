2c5ea0e48a37502a8f5e4460cb33e41b98e39f5f
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-10-01 23:22:05 +0600

    ADDED theming

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var btn = document.getElementById("submit");
+btn.addEventListener("click", request);
 
-btn.addEventListener("click", func);
-function func() {
-let username = document.getElementById("username").value;
-  console.log(username.value);
+function request() {
+  let username = document.getElementById("username").value;
 
   if (username !== "") {
+
     const Http = new XMLHttpRequest();
     const url = "https://www.instagram.com/" + username + "/?__a=1";
     Http.open("GET", url);
@@ -15,11 +15,6 @@ let username = document.getElementById("username").value;
       var res = Http.responseText;
       var prettyJSON = JSON.parse(res);
       var json = JSON.stringify(prettyJSON, null, 2);
-
-      console.log(
-        "Followers: " + prettyJSON.graphql.user.edge_followed_by.count
-      );
-      console.log(json);
       var info = document.getElementById("json");
       info.innerHTML = json;
     };
